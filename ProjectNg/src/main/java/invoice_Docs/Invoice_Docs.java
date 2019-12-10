28e1cca892be784d7368d9c67cb77d9a8e3b215b
Chandranarayana Iragala <chandranarayana.iragala@factset.com>
2019-12-10 22:34:49 +0530

    Added one Test Case named with invoice_Docs_TC2

---
+++ ProjectNg/src/main/java/invoice_Docs/Invoice_Docs.java
@@ -0,0 +1,16 @@
+package invoice_Docs;
+
+import org.testng.annotations.Test;
+
+public class Invoice_Docs {
+
+	@Test
+	public void invoice_Docs_TC1(){
+		System.out.println("invoice_Docs_TC1");
+	}
+	
+	@Test
+	public void invoice_Docs_TC2(){
+		System.out.println("invoice_Docs_TC1");
+	}
+}
